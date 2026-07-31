39de99ff574381958e5b956065601d97dbe0ca3d
YALLICO OWNER <yallico2024@gmail.com>
2026-07-31 08:10:03 -0500

    Actualizar _audiobye.js

--- plugins/_audiobye.js
+++ plugins/_audiobye.js
@@ -3,24 +3,28 @@ let handler = async (m, { conn, isAdmin }) => {
 │ 😡 *SOLO ADMINS*
 ╰─────────────────🐉`);
 
-  if (!m.quoted ||!m.quoted.audio) return m.reply(`╭─🐉 *『 𝗦𝗢𝗡 𝗚𝗢𝗞𝗨 𝗣𝗥𝗘𝗠 』* 🐉─╮
+  if (!m.quoted) return m.reply(`╭─🐉 *『 𝗦𝗢𝗡 𝗚𝗢𝗞𝗨 𝗣𝗥𝗘𝗠 』* 🐉─╮
 │ ⚠️ *RESPONDE A UN AUDIO*
+╰─────────────────🐉`);
+
+  let q = m.quoted
+  let mime = (q.msg || q).mimetype || ''
+  if (!mime ||!mime.includes('audio')) return m.reply(`╭─🐉 *『 𝗦𝗢𝗡 𝗚𝗢𝗞𝗨 𝗣𝗥𝗘𝗠 』* 🐉─╮
+│ ⚠️ *ESO NO ES UN AUDIO*
 │
-│ *Manda o responde a una nota de voz/audio*
+│ *Responde a una nota de voz o audio*
 ╰─────────────────🐉`);
 
   let chat = global.db.data.chats[m.chat]
   if (!chat) global.db.data.chats[m.chat] = {}
   chat = global.db.data.chats[m.chat]
 
-  let url = await conn.downloadAndSaveMediaMessage(m.quoted)
+  let url = await conn.downloadAndSaveMediaMessage(q)
   chat.byeAudio = url
   await global.db.write()
 
   m.reply(`╭─🐉 *『 𝗦𝗢𝗡 𝗚𝗢𝗞𝗨 𝗣𝗥𝗘𝗠 』* 🐉─╮
 │ 💀 *AUDIO DE DESPEDIDA GUARDADO*
-│
-│ *Cada vez que salga alguien sonara este audio*
 ╰─────────────────🐉`);
 }
 handler.help = ['audiobye']
