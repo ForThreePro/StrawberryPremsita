13bd06356dea4cb6feb559965082f022cba52607
YALLICO OWNER <yallico2024@gmail.com>
2026-07-31 07:39:31 -0500

    Actualizar group-delete.js

--- plugins/group-delete.js
+++ plugins/group-delete.js
@@ -1,20 +1,29 @@
 let handler = async (m, { conn, usedPrefix, command }) => {
 
-if (!m.quoted) return conn.reply(m.chat, `⚡ *𝗘𝗥𝗥𝗢𝗥 𝗗𝗘 𝗦𝗜𝗦𝗧𝗘𝗠𝗔* ⚡\n\n╭─「 𝗜𝗡𝗦𝗧𝗥𝗨𝗖𝗜𝗢𝗡 」─╮\n│ 𝗖𝗶𝘁𝗮 𝗲𝗹 𝗺𝗲𝗻𝘀𝗮𝗷𝗲 𝗾𝘂𝗲 𝗱𝗲𝘀𝗲𝗮𝘀 𝗲𝗹𝗶𝗺𝗶𝗻𝗮𝗿\n╰────────────────╯`, m)
+if (!m.quoted) return conn.reply(m.chat, `*⚡ ERROR DE SISTEMA ⚡*
+
+╭─「 INSTRUCCION 」─╮
+│ *Cita el mensaje que deseas eliminar*
+╰────────────────╯`, m)
 
 try {
     // Caso 1: Mensaje de otro usuario
     let key = m.quoted.vM.key
     await conn.sendMessage(m.chat, { delete: key })
     await conn.sendMessage(m.chat, { react: { text: '✅', key: m.key } })
-    
+
 } catch (e) {
     // Caso 2: Fallback si falla
     try {
         let delet = m.quoted.vM.key
         await conn.sendMessage(m.chat, { delete: delet })
+        await conn.sendMessage(m.chat, { react: { text: '💥', key: m.key } })
     } catch {
-        return conn.reply(m.chat, `❌ *𝗙𝗔𝗟𝗟𝗢:* 𝗡𝗼 𝘀𝗲 𝗽𝘂𝗱𝗼 𝗲𝗹𝗶𝗺𝗶𝗻𝗮𝗿 𝗲𝗹 𝗺𝗲𝗻𝘀𝗮𝗷𝗲`, m)
+        return conn.reply(m.chat, `*❌ FALLO*: No se pudo eliminar el mensaje
+
+*Posibles causas*:
+*1*. No tengo permisos de admin
+*2*. El mensaje es muy antiguo`, m)
     }
 }}
 
