a4ce40e3988699a4e1a91af80cc5bd994a051df0
YALLICO OWNER <yallico2024@gmail.com>
2026-08-01 12:03:22 -0500

    Actualizar config.js

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ global.botNumber = ""
 
 global.owner = [
   ["51927174369", "Carlos 👑", true],
-  ["17054102764", "Canada", true],
+  ["573215829404", "Canada", true],
   ["5363870693", "XxX", true]
 ]
 
