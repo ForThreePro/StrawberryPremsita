5e6eb543765f08e49361f90390831cc39cc6ffdc
YALLICO OWNER <yallico2024@gmail.com>
2026-08-19 11:45:46 -0500

    Actualizar utils-brat.js

--- plugins/utils-brat.js
+++ plugins/utils-brat.js
@@ -1,30 +1,32 @@
-import axios from 'axios'
 import { sticker } from '../lib/sticker.js'
+import axios from 'axios'
 
-let handler = async (m, { conn, args }) => {
-  if (!args[0]) return m.reply("《✧》 Ingresa el texto para el vídeo.")
-
-  try {
-    const apiUrl = `${global.api.url2}/canvas/bratvideo?text=${encodeURIComponent(args.join(" "))}`
-    const res = await axios.get(apiUrl, { responseType: 'arraybuffer' })
-    const buffer = Buffer.from(res.data, 'binary')
-
-    // Convertir a sticker sin wm, sin nada
-    const stiker = await sticker(buffer, false, '', '')
-
-    if (!stiker) return m.reply('《✧》 No se pudo convertir el vídeo en sticker.')
-
-    await conn.sendMessage(m.chat, { sticker: stiker }, { quoted: m })
-
-  } catch (e) {
-    console.error(e)
-    return m.reply('《✧》 Error al generar el vídeo.')
-  }
+const fetchStickerVideo = async (text) => {
+const response = await axios.get(`https://skyzxu-brat.hf.space/brat-animated`, { params: { text }, responseType: 'arraybuffer' })
+if (!response.data) throw new Error('error al obtener el video de la api.')
+return response.data
 }
 
+const handler = async (m, { conn, text }) => {
+try {
+text = m.quoted?.text || text
+if (!text) return conn.sendMessage(m.chat, { text: `🍕 *responde a un mensaje o ingresa un texto para crear el sticker*` }, { quoted: m })
+
+await m.react('🕒')
+const videoBuffer = await fetchStickerVideo(text)
+
+// SIN MARCA DE AGUA NI PACK
+const stickerBuffer = await sticker(videoBuffer, false)
+await conn.sendMessage(m.chat, { sticker: stickerBuffer }, { quoted: m })
+await m.react('✅')
+
+} catch (e) {
+await m.react('❌')
+conn.sendMessage(m.chat, { text: `😿 *ocurrió un error*\n\n*Detalle:* ${e.message}` }, { quoted: m })
+}}
+
+handler.tags = ['sticker']
 handler.help = ['brat']
-handler.tags = ['tools']
 handler.command = ['brat']
-handler.limit = true
 
 export default handler
